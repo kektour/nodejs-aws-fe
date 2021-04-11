1e7f223edadc60e36d4ddf07d5f013c78eae0be9
Aliaksei Rubanik <Aliaksei_Rubanik@epam.com>
2021-04-11 21:57:22 +0300

    implemented task

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -1,17 +1,17 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
-import {Product} from "models/Product";
-import {formatAsPrice} from "utils/utils";
-import AddProductToCart from "components/AddProductToCart/AddProductToCart";
-// import axios from 'axios';
-// import API_PATHS from "constants/apiPaths";
-import productList from "./productList.json";
+import axios from 'axios';
+
+import AddProductToCart from 'components/AddProductToCart/AddProductToCart';
+import API_PATHS from 'constants/apiPaths';
+import { Product } from 'models/Product';
+import { formatAsPrice } from 'utils/utils';
 
 const useStyles = makeStyles((theme) => ({
   card: {
@@ -36,10 +36,8 @@ export default function Products() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    // axios.get(`${API_PATHS.bff}/product/available/`)
-    //   .then(res => setProducts(res.data));
-    setProducts(productList);
-  }, [])
+    axios.get(`${API_PATHS.bff}/product-list`).then((res) => setProducts(res.data.products));
+  }, []);
 
   return (
     <Grid container spacing={4}>
