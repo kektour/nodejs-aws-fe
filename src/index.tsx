9fa64953ea1187a5ecaa09422778af4cf89c631f
Aliaksei Rubanik <Aliaksei_Rubanik@epam.com>
2021-06-07 00:35:06 +0300

    added base authorization

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'index.css';
 import App from 'components/App/App';
-import {store} from 'store/store';
-import {Provider} from 'react-redux';
+import { store } from 'store/store';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import CssBaseline from "@material-ui/core/CssBaseline";
+import CssBaseline from '@material-ui/core/CssBaseline';
 import axios from 'axios';
 
 axios.interceptors.response.use(
-  response => {
-    return response;
-  },
-  function(error) {
-    if (error?.response?.status === 400) {
+  (response) => response,
+  (error) => {
+    const status = error?.response?.status;
+
+    if (status === 400) {
       alert(error.response.data?.data);
+    }
+
+    if (status === 401 || status === 403) {
+      alert(error.response.data.message);
     }
 
     return Promise.reject(error?.response ?? error);
@@ -24,8 +28,8 @@ axios.interceptors.response.use(
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <CssBaseline/>
-      <App/>
+      <CssBaseline />
+      <App />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
