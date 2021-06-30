0b21c68f3e86fac0410b092a8a25c46f8aa51a14
Aliaksei Rubanik <Aliaksei_Rubanik@epam.com>
2021-07-01 02:32:06 +0300

    added cart api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://914rrj5nrf.execute-api.us-east-1.amazonaws.com/dev',
   bff: 'https://6e7mkgdxi5.execute-api.us-east-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://kektour-cart-api-dev.us-west-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
